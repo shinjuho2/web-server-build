5f57b0e19e955b95caf00f360cda6d9a82c876ab
shinjuho2 <sinjuho2@gmail.com>
2024-02-27 19:48:41 +0900

    service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: Array<User> = [];
+  private id = 0;
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    this.users.push({ id: ++this.id, ...createUserDto, createdAt: new Date() });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return [...this.users];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const found = this.users.find( (u) => u.id === id );
+    if(!found) throw new NotFoundException();
+    return found;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
